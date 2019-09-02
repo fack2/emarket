efafc728599b5118cc56deabc051248d434a7918
YousefQwasmeh <151037@ppu.edu.ps>
2019-09-02 15:23:00 +0300

    Separate dummy file

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -1,16 +1,25 @@
-const fs = require('fs')
+const fs = require('fs');
 
-const dbConnection = require('./db_connection')
+const dbConnection = require('./db_connection');
 
-const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString()
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+const sql2 = fs.readFileSync(`${__dirname}/initValue.sql`).toString();
 
-const runDbBuild = cb => dbConnection.query(sql, cb)
+const runDbBuild = (cb) => dbConnection.query(sql, cb);
+
+const runDbBuild2 = (cb) => dbConnection.query(sql2, cb);
 
 runDbBuild((err, res) => {
   if (err) {
-    throw err
+    throw err;
   }
-  return res
-})
+  return res;
+});
+runDbBuild2((err, res) => {
+  if (err) {
+    throw err;
+  }
+  return res;
+});
 
-module.exports = runDbBuild
+module.exports = {runDbBuild, runDbBuild2};
