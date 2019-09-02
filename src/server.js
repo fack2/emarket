2597ff988ac3d3bd36685395eb9f42ac26906706
Muniralsharif <munir.sniper@gmail.com>
2019-09-02 15:02:22 +0300

    fixed eslint

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const app = require('./app.js')
-const PORT =process.env.PORT || 5555
+const PORT = process.env.PORT || 5555
 app.listen(PORT, () => {
-  console.log(`Our app is running on http://localhost:${PORT}`);
-});
+  console.log(`Our app is running on http://localhost:${PORT}`)
+})
