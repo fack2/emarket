0494aa665506bbbf61838f2a5cb6a66783abd007
Muniralsharif <munir.sniper@gmail.com>
2019-09-03 09:22:37 +0300

    solve the indentaion and the white spaces

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from '../Home'
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      <Route exact path="/" component={Home}/>
-    </div>
-    </Router>
-  );
+
+function App () {
+  return (< Router >
+    <div className = "App" >
+      <Route exact path = "/"
+        component = { Home }/>
+
+    </div> </Router >
+
+  )
 }
 
-export default App;
+export default App
--- client/src/Navbar/index.js
+++ client/src/Navbar/index.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import './style.css'
-const NavBar =()=>{
+const NavBar = () => {
   return (
-    <div className="nav-bar">
-      
-    <section className="logo-section">
-  <img className="logo" src="https://3.top4top.net/p_1339rel6k1.png" alt="logo"/>
-  <p className="shop">Shop</p>
-  </section>
-  <section className="cart-section">
-   <img className="cart" src="https://2.top4top.net/p_1339xtovq1.png" alt="cart"/>
-  <img className="menu" src="https://4.top4top.net/p_1339ilo061.png" alt="menu"/>
-  </section>
-  
+    <div className = "nav-bar">
 
-  </div>
+      <section className = "logo-section" >
+        <img className = "logo"
+          src = "https://3.top4top.net/p_1339rel6k1.png"
+          alt = "logo" / >
+        <p className = "shop" > Shop </p> < /section >
+      <section className = "cart-section" >
+        <img className = "cart"
+          src = "https://2.top4top.net/p_1339xtovq1.png"
+          alt = "cart" / >
+        <img className = "menu"
+          src = "https://4.top4top.net/p_1339ilo061.png"
+          alt = "menu" / >
+      </section>
+
+    </div>
   )
 }
 export default NavBar
