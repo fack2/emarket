9cb16f1440f3417248902ffa8ff877e9d99e968b
Muniralsharif <munir.sniper@gmail.com>
2019-09-03 10:09:38 +0300

    change to snake case for init_value.sql

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -3,16 +3,16 @@ const path = require('path');
 
 const dbConnection = require('./db_connection');
 
-const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString();
-const sql2 = fs.readFileSync(path.join(__dirname, 'initValue.sql')).toString();
+const sql = fs.readFileSync(path.join(__dirname, 'db_build.sql')).toString()
+const sql2 = fs.readFileSync(path.join(__dirname, 'init_value.sql')).toString()
 
 const runDbBuild = (sql, cb) => dbConnection.query(sql, cb);
 
 const cb = (err, res) => {
-  if (err) {
-    throw err;
-  }
-  return res;
+    if (err) {
+        throw err;
+    }
+    return res;
 };
 runDbBuild(sql, cb);
 runDbBuild(sql2, cb);
