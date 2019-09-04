ffca551731b7074cb5cd45edb5dca9188668f663
someyaaltous <someyaaltous1995@gmail.com>
2019-09-04 14:30:19 +0300

    solve comments

--- client/src/components/Category/index.js
+++ client/src/components/Category/index.js
@@ -6,28 +6,32 @@ class Category extends React.Component {
     result: []
   };
   componentDidMount() {
-    axios
-      .get("/category")
-      .then(data => {
-        return data;
-      })
-      .then(data => {
-        const resultArray = data.data.data.map(e => e);
-        this.setState({ result: resultArray });
-      });
+    axios.get("/category").then(result => {
+      const resultArray = result.data.map(e => e);
+      this.setState({ result: resultArray });
+    });
   }
 
   render() {
     return (
       <div className="grid-container">
-        {this.state.result.map(e => (
-          <div onClick={this.props.onClick}>
-            <img className="grid-item" src={e.img} />
-            <p className="category">{e.name}</p>
-          </div>
-        ))}
+        {!this.state.result.length ? (
+          <img
+            className="loading"
+            src="https://media1.tenor.com/images/556e9ff845b7dd0c62dcdbbb00babb4b/tenor.gif?itemid=5300368"
+            alt="loading"
+          />
+        ) : (
+          this.state.result.map(e => (
+            <div onClick={this.props.onClick}>
+              <img className="grid-item" src={e.img} />
+              <p className="category">{e.name}</p>
+            </div>
+          ))
+        )}
       </div>
     );
   }
 }
 export default Category;
+// this.state.result?(this.state.map(e=>...):<h2>Loading</h2>
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,25 +1,23 @@
-import React from "react";
-import NavBar from "../Navbar";
-import "./style.css";
-import Category from "../Category";
-class Home extends React.Component {
-  goToShop = () => {
-    const { history } = this.props;
-    history.push("/shop");
-  };
-  render() {
-    return (
-      <div className="home">
-        <NavBar />
-        <img
-          className="main-img"
-          src="https://5.top4top.net/p_1341td5iv1.jpg"
-          alt="main image"
-        />
-        <Category onClick={this.goToShop} />
-      </div>
-    );
-  }
+import React from 'react'
+import NavBar from '../Navbar'
+import './style.css'
+import Category from '../Category'
+
+const Home = (props) => {
+  return (
+    <div className="home">
+      <NavBar />
+      <img
+        className="main-img"
+        src="https://5.top4top.net/p_1341td5iv1.jpg"
+        alt="main image"
+      />
+      <Category onClick={() => {
+        const { history } = props
+        history.push('/shop/:category_id')
+      }} />
+    </div>
+  )
 }
 
-export default Home;
+export default Home
