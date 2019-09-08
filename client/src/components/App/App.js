2e1699f663a77f7b7eadc41d1dc98c33bb9a6f3c
someyaaltous <someyaaltous1995@gmail.com>
2019-09-08 10:52:41 +0300

    changed the file from function to class Complete FE

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from '../Home'
+import shippingInfo from '../shipping_info'
 
 function App () {
   return (< Router >
     <div className = "App" >
       <Route exact path = "/"
         component = { Home }/>
+      <Route exact path = "/shipping_info" component={shippingInfo}/>
 
     </div> </Router >
 
