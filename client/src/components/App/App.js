5ef7ce53bd4efda34f74341604c050672c82be53
someyaaltous <someyaaltous1995@gmail.com>
2019-09-08 13:42:08 +0300

    added shipping info in App.js

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,15 +2,15 @@ import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from '../Home'
-
+import shippingInfo from '../shipping_info'
 function App () {
-  return (< Router >
-    <div className = "App" >
-      <Route exact path = "/"
-        component = { Home }/>
-
-    </div> </Router >
-
+  return (
+    <Router>
+      <div className="App">
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shipping_info" component={shippingInfo} />
+      </div>{' '}
+    </Router>
   )
 }
 
