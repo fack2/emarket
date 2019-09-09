de6870667aa1cdc35359c2d237cad481e31e89ef
YousefQwasmeh <151037@ppu.edu.ps>
2019-09-09 16:09:54 +0300

    remove more comments

--- client/src/components/Shop/index.js
+++ client/src/components/Shop/index.js
@@ -4,36 +4,23 @@ import SearchBar from '../Search_bar'
 import React, { Component } from 'react'
 import './style.css'
 
-
 class Shop extends Component {
   state = {
-
-resultProducts:[]
-
+    resultProducts: []
   }
-  searchSetArray = (resultProducts) =>{
-
-this.setState({resultProducts})
-
+  searchSetArray = resultProducts => {
+    this.setState({ resultProducts })
   }
   render() {
     return (
       <div className="shop-page">
-      <NavBar />
-       {/* <SearchBar  {...this.props} serchArray={this.searchSetArray} />
-{!this.state.resultProducts.length ? (
-  <ProductCard resultAfterSerch = {this.state.resultProducts} />
+        <NavBar />
 
-}):  (<ProductCard {...this.props} />)
-    } */}
-    <SearchBar  {...this.props} serchArray={this.searchSetArray} />
-<ProductCard {...this.props} />
-    </div>
-    ) 
+        <SearchBar {...this.props} serchArray={this.searchSetArray} />
+        <ProductCard {...this.props} />
+      </div>
+    )
   }
 }
 
 export default Shop
-
-
-
