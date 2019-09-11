a050c313abcc4e2baa50c4bb005dbe472a7fdd76
someyaaltous <someyaaltous1995@gmail.com>
2019-09-11 11:12:51 +0300

    solve comment

--- src/controller/add_order.js
+++ src/controller/add_order.js
@@ -2,7 +2,7 @@ const postOrder = require('../database/queries/post_order')
 
 const addOrder = (req, res) => {
   const { username, phone, Address, Extra_Note, orderID = 3 } = req.body
-  // orderID token from cookie
+  // The orderID will be token from the cookie after making the login page
 
   postOrder(username, phone, Address, Extra_Note, orderID)
     .then(() => res.status(200).json({ res: 'success true' }))
