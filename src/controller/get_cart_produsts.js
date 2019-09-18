308683a9d8b7d66651e0cc3267acb44b04411eab
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-18 11:15:01 +0300

    resolve

--- src/controller/get_cart_produsts.js
+++ src/controller/get_cart_produsts.js
@@ -2,7 +2,6 @@ const getCartProducts = require('../database/queries/get_cart_products')
 
 exports.getProducts = (request, response) => {
   const orderID = 1
-  console.log('000000000000000000000000')
   getCartProducts(orderID)
     .then(result => response.json(result))
     .catch(err => response.json(err))
