ce4100bda7af5fab5453b5a529607ddd94a0b799
nadeen123 <nadeenshaheen.shahin@gmail.com>
2019-09-18 11:28:05 +0300

    resolve name spelling

--- src/controller/getCartProdusts.js
+++
@@ -1,8 +0,0 @@
-const getCartProducts = require('../database/queries/getCartProducts')
-
-exports.getProducts = (request, response) => {
-  const orderID = 1
-  getCartProducts(orderID)
-    .then(result => response.json(result))
-    .catch(err => response.json(err))
-}
