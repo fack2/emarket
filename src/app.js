99ec5b8720eb130cc03f159a9fc0a24c325c1e6e
YousefQwasmeh <151037@ppu.edu.ps>
2019-11-29 01:56:36 +0200

    change / to * in controler

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-const path = require('path')
-const express = require('express')
-const app = express()
-const router = require('./controller/index')
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
-app.use(express.json())
-app.use(express.urlencoded())
+const path = require("path");
+const express = require("express");
+const app = express();
+const router = require("./controller/index");
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.use(router)
+app.use(router);
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
-})
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
-module.exports = app
+module.exports = app;
